bd08b6e4cb4ddd5b2a5e3eef9ebd2af9bd54c343
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-21 05:13:28 -0300

    Conflict, accepted current change (API ID)

--- modules/API/API_Data.js
+++ modules/API/API_Data.js
@@ -1,8 +1,4 @@
 const urlAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-<<<<<<< HEAD
 const gameIdAPI = 'WwsqXQp17V5N3vZdcYtC';
-=======
-const gameIdAPI = 'eFVt5KgRAI2pShctfdlG';
->>>>>>> develop
 
 export { urlAPI, gameIdAPI };
